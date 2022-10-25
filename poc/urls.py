b6c47abdac1b275f0ce46572c368636362245c72
zenderro <zenderro@gmail.com>
2022-10-25 10:59:25 +0300

    django 2.2 support

--- poc/urls.py
+++ poc/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^select2/', include('django_select2.urls')),
     url(r'^', include('poc.app.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
